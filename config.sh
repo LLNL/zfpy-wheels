e2e73475e307024e2afe9bd415bfc2ae5035cb69
David Wade <dawad@equinor.com>
2020-03-24 13:22:44 +0000

    Correct paths

--- config.sh
+++ config.sh
@@ -4,17 +4,35 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    mkdir build
-    pushd build
-    which python
+
+    $PYTHON_EXE -m pip install numpy cython scikit-build "cmake<3.14"
+    PYTHON_LIB=$($PYTHON_EXE -c "from distutils import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+    PYTHON_INCLUDE=$($PYTHON_EXE -c "from sysconfig import get_paths; print(get_paths()['include'])")
     NUMPY_INCLUDE=$($PYTHON_EXE -c "import numpy; print(numpy.get_include())")
-    cmake .. -DBUILD_ZFPY=ON -DPYTHON_NUMPY_INCLUDE_DIR=$NUMPY_INCLUDE
+
+    echo $PYTHON_EXE
+    echo $PYTHON_LIB
+    echo $PYTHON_INCLUDE
+    echo $NUMPY_INCLUDE
+    $PYTHON_EXE --version
+
+
+    mkdir zfp/build
+    pushd zfp/build
+
+    cmake --version
+
+    cmake .. -DBUILD_ZFPY=ON -DPYTHON_EXECUTABLE=$PYTHON_EXE -DPYTHON_LIBRARY=$PYTHON_LIB -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE -DPYTHON_NUMPY_INCLUDE_DIR=$NUMPY_INCLUDE
+
+    echo "Build configured"
+
     make
+
     popd
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    # python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
+    python zfp/tests/python/test_numpy.py
 }
